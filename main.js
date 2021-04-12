1aeba3bb0ac6c2dd0ad764d5c60103d0ce589bfe
Brandon Chansamone <braakson@yahoo.com>
2021-04-12 16:28:58 -0600

    Fixed some bugs and reformatted code

--- main.js
+++ main.js
@@ -1,46 +1,66 @@
-const express= require("express"), app=express(),
-homeController=require("./controllers/homeController"),
+"use strict";
+
+
+const express = require("express"), 
+app = express(),
+router = express.Router(),
+homeController = require("./controllers/homeController"),
 errorController = require("./controllers/errorController"),
-subscribersController = require("./controllers/subscribersController"),
+layouts = require("express-ejs-layouts"), 
+mongoose = require("mongoose"),
 methodOverride = require("method-override"),
-layouts = require("express-ejs-layouts"),mongoose=require("mongoose");
+subscribersController = require("./controllers/subscribersController"),
+usersController = require("./controllers/usersController"),
+coursesController = require("./controllers/coursesController");
 
-mongoose.connect("mongodb://localhost:27017/confetti_cuisine",
-{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/confetti_cuisine", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
 
-
-
-app.set("port", process.env.PORT || 3001);
-
+app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
-app.use(layouts);
 
-app.get("/", homeController.showIndex);
-
-app.use(express.static("public"))
-app.use(
+router.use(express.static("public"));
+router.use(layouts);
+router.use(
     express.urlencoded({
-        extended:false
+        extended: false
     })
 );
 
-app.use(express.json());
+router.use(methodOverride("_method", {methods: ['POST', 'GET']}));
+router.use(express.json());
 
-app.use(methodOverride("_method", {methods: ["POST", "GET"]}));
+router.get("/", homeController.index);
 
-app.get("/courses", homeController.showCourses);
-app.get("/subscribers", subscribersController.getAllSubscribers);
-app.get("/contact", subscribersController.getSubscriptionPage);
-app.post("/subscribe",subscribersController.saveSubscriber);
+router.get("/subscribers", subscribersController.index, subscribersController.indexView);
+router.get("/subscribers/new", subscribersController.new);
+router.post("/subscribers/create", subscribersController.create, subscribersController.redirectView);
+router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
+router.get("/subscribers/:id/edit", subscribersController.edit);
+router.put("/subscribers/:id/update", subscribersController.update, subscribersController.redirectView);
+router.delete("/subscribers/:id/delete", subscribersController.delete, subscribersController.redirectView);
 
-// app.get("/contact", homeController.showSignUp);
-// app.post("/contact", homeController.postedSignUpForm);
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.post("/users/create", usersController.create, usersController.redirectView);
+router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update", usersController.update, usersController.redirectView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
 
-app.use(errorController.pageNotFoundError);
-app.use(errorController.interalServerError);
+router.get("/courses", coursesController.index, coursesController.indexView);
+router.get("/courses/new", coursesController.new);
+router.post("/courses/create", coursesController.create, coursesController.redirectView);
+router.get("/courses/:id", coursesController.show, coursesController.showView);
+router.get("/courses/:id/edit", coursesController.edit);
+router.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
+router.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
 
-app.listen(app.get("port"), () =>{
-    console.log(`Server is running on port: ${app.get("port")}`)
+router.use(errorController.pageNotFoundError);
+router.use(errorController.internalServerError);
 
+app.use("/", router);
+
+app.listen(app.get("port"), () => {
+    console.log(`Server is running on port: ${app.get("port")}`);
 });
-
